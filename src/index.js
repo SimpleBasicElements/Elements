28ce27d83d1971e5bc63a7850e138f12dd9924fe
Jeremy Smith <contact@swith.fr>
2020-04-03 15:38:24 +0200

    Missing export
    Add missing export for modal-dialog export for ModalDialog CustomElement.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as NavTabs } from './nav-tabs/index.js'
 export { default as ScrollTop } from './scroll-top/index.js'
+export { default as ModalDialog } from './modal-dialog/index.js'
